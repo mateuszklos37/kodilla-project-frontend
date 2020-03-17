1fce84ec219dc9efb91a33aabf62e3ec2cacf658
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2020-03-17 22:15:15 +0100

    Initial commit

---
+++ src/main/java/kodilla/frontend/kodillafrontend/KodillaFrontendApplication.java
@@ -0,0 +1,13 @@
+package kodilla.frontend.kodillafrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaFrontendApplication.class, args);
+    }
+
+}
